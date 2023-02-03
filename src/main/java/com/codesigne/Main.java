98f757f2a08f7f2f84dc365030431d6fde3dbff9
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2023-02-03 01:44:42 +0100

    build : make some test

--- src/main/java/com/codesigne/Main.java
+++ src/main/java/com/codesigne/Main.java
@@ -1,16 +1,47 @@
 package com.codesigne;
 
 import com.codesigne.model.BankAccount;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
 
-        BankAccount bankAccount = null;
-        bankAccount = new BankAccount();
-        bankAccount.accoundId = "123-456";
-        bankAccount.balance = 2324;
-        bankAccount.currency = "MAD";
+        HashMap<String,BankAccount> bankAccountHashMap = new HashMap<>();
+        //fill hashmap
+        bankAccountHashMap.put("cc1",new BankAccount());
+        bankAccountHashMap.put("cc2",new BankAccount());
+        bankAccountHashMap.put("cc3",new BankAccount());
+        bankAccountHashMap.put("cc4",new BankAccount());
+        bankAccountHashMap.put("cc5",new BankAccount());
 
-        System.out.println("Account Id = "+bankAccount.accoundId);
+        //print al data
+        System.out.println("=========================Data========================");
+        for(BankAccount ba : bankAccountHashMap.values()){
+            System.out.println(toJson(ba));
+        }
+
+
+
+
+    }
+
+    //create function to fetch data format json
+    public static String toJson(BankAccount o) throws JsonProcessingException {
+        ObjectMapper  objectMapper = new ObjectMapper();
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(o);
+    }
+
+    public static void printAcoount(BankAccount bankAccount){
+        System.out.println("*****************************************************");
+        System.out.println("Account Id = "+bankAccount.getAccoundId());
+        System.out.println("Balance = "+bankAccount.getBalance());
+        System.out.println("Currency = "+bankAccount.getCurrency());
+        System.out.println("Status = "+bankAccount.getStatus());
+        System.out.println(bankAccount.equals(bankAccount));
+        System.out.println(bankAccount.hashCode());
+        System.out.println("*****************************************************");
     }
 }
